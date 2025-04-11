e329b8428c1f5d4fea9ec7e48d66c90e242a7849
Daniel <daniel.reboucas@loft.com.br>
2025-04-11 09:53:40 -0300

    a

--- src/index.php
+++ src/index.php
@@ -1,26 +1,27 @@
 <?php
 
-function hello() {
+function hello() 
+{
     echo "hello";
 }
 
-function hello2() 
+function hello2()
 {
     echo "hello2";
 }
 
 
-function hello3() {
+function hello3()
+{
     echo "hello2";
 }
 
-function hello4() {
+function hello4() 
+{
     echo "hello2";
 }
 
-function hello5() {
+function hello5() 
+{
     echo "hello2";
 }
-
-
-echo "hi friens\n";
